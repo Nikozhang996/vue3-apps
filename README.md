7b9adfdcd6b4e76533642b23ed8a551ea9e1fa37
NikoZhang <593177876@qq.com>
2020-11-05 22:47:14 +0800

    feat: home models

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-vue-apps
--
+## vue-apps
+
 https://gitee.com/jw-speed/zhufeng-public/tree/master/zhufeng-ketang-ts-2
 
 ### 疑点收集
+
 - 枚举
 - 元祖
-- vue,vuex,vue-router新版本各项api
+- vue,vuex,vue-router 新版本各项 api
 
 ### 收获
-
--- src/store/modules/home.ts
+++ src/store/modules/home.ts
@@ -1,5 +1,5 @@
 import { Module } from "vuex";
-import { GlobalState } from "..";
+import store, { GlobalState } from "..";
 import * as Types from "../action-types";
 import { CATOGORY_TYPES, HomeState, Lesson, Lessons, Slider } from "@/typings";
 
@@ -11,15 +11,57 @@ const state: HomeState = {
     loading: false,
     offset: 0,
     limit: 5,
-    list: []
-  }
+    list: [],
+  },
 };
 
 const home: Module<HomeState, GlobalState> = {
   namespaced: true,
   state,
-  mutations:{},
-  actions:{},
+  mutations: {
+    [Types.SET_CATEGORY](state, payload: CATOGORY_TYPES) {
+      state.currentCategory = payload;
+    },
+    [Types.SET_SLIDER_LIST](state, payload: Slider[]) {
+      state.sliders = payload;
+    },
+    [Types.SET_LOADING](state, payload: boolean) {
+      state.lessons.loading = payload;
+    },
+    [Types.SET_LESSON_LIST](state, payload: Lessons) {
+      state.lessons.list = [...state.lessons.list, ...payload.list];
+      state.lessons.hasMore = payload.hasMore;
+      state.lessons.offset = state.lessons.offset + payload.list.length;
+    },
+  },
+  actions: {
+    async [Types.SET_SLIDER_LIST]({ commit }) {
+      const sliders: any = [];
+      commit(Types.SET_SLIDER_LIST, sliders);
+      // const sliders = await getSliders<Sliders>();
+    },
+    async [Types.SET_LESSON_LIST]({ commit }) {
+      if (state.lessons.loading === true) {
+        return;
+      }
+      if (state.lessons.hasMore === false) {
+        return;
+      }
+
+      commit(Types.SET_LOADING, true);
+
+      const lessons: any = {
+        list: [],
+      };
+      /* const lessons = await getLessons<lessons>(
+        state.currentCategory,
+        state.lessons.offset
+      );
+      */
+      commit(Types.SET_LESSON_LIST, lessons);
+      commit(Types.SET_LOADING, false);
+    },
+  },
 };
 
 export default home;
